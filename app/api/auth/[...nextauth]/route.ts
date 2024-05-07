9c9bb7e843d77d6f54de224aa2368f76971699ea
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-05-07 20:31:45 +0100

    endpoint for next-auth provider

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,61 @@
+import { connectMongoDB } from "@/lib/mongodb";
+import User from "@/models/user";
+import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import { Session } from "next-auth";
+
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {},
+
+      async authorize(credentials) {
+        const { email, password } = credentials as {
+          email: string,
+          password: string
+        };
+
+        try {
+          await connectMongoDB();
+          const user = await User.findOne({ email });
+
+          if (!user) {
+            return null;
+          }
+
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+
+          if (!passwordsMatch) {
+            return null;
+          }
+
+          return user;
+        } catch (error) {
+          console.log("Error: ", error);
+        }
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/",
+  },
+  callbacks: {
+    async session({ session }: { session: Session }) {
+      const sessionUser = await User.findOne({ email: session.user.email });
+      if (sessionUser) {
+        session.user.id = sessionUser._id.toString();
+        return session;                
+      } 
+    },
+  },
+};
+
+const handler = NextAuth(authOptions as any);
+
+export { handler as GET, handler as POST };
