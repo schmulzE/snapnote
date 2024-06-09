5492c9415c9a616b35867eacd48c18fbf1598e75
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-06-09 08:24:59 +0100

    increaed the lightness of autogenerated colors

--- utils/generateColorFromText.ts
+++ utils/generateColorFromText.ts
@@ -1,15 +1,15 @@
-export default function generateColorFromText(text: string) {
+export default function generateColorFromText(text: string| undefined) {
   let hash = 0;
 
   // Convert the input text to a unique hash value
-  for (let i = 0; i < text.length; i++) {
-    hash = text.charCodeAt(i) + ((hash << 5) - hash);
+  for (let i = 0; i < text!.length; i++) {
+    hash = text!.charCodeAt(i) + ((hash << 5) - hash);
   }
 
   // Generate a color value based on the hash value
   let hue = (hash % 360) / 360; // Calculate hue from the hash value
   let saturation = 0.8; // Increase saturation to 80%
-  let lightness = 0.6; // Increase lightness to 60%
+  let lightness = 0.8; // Increase lightness to 80%
 
   const color = `hsl(${hue * 360}, ${saturation * 100}%, ${lightness * 100}%)`;
 
