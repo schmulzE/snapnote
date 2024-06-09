067ac1e1ba9fe7752f52589693d31e7c96dd4802
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-06-09 08:31:25 +0100

    renamed folder to 'components'

--- app/(components)/listBoxWrapper.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ReactNode } from "react";
-
-const ListboxWrapper = ({children} : {children : ReactNode}) => (
-  <div className="w-full max-w-[260px] rounded-small dark:border-default-100">
-    {children}
-  </div>
-);
-
-export default ListboxWrapper;
--- app/(components)/ui/checkbox.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import {Checkbox} from "@nextui-org/react";
-
-export default function App({children} : {children : React.ReactNode}) {
-  return (
-    <Checkbox defaultSelected lineThrough>{children}</Checkbox>
-  );
-}
