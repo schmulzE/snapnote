57d88517449626565d136c7ebb0f2473d42634b1
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-11-21 23:35:24 +0100

    feat: Implemented TagItem component for displaying and deleting tags
    * Added TagItem component for displaying individual tags.
    * Shows tag name, generated color icon, and a delete button (if applicable).
    * Uses Link for navigation to tag details page.
    * Implements confirmation dialog before deleting a tag.
    * Triggers deleteTag action to remove the tag on the server.
    * Uses useRouter for client-side route refresh after deletion (optional).

---
+++ app/components/tags/tagItem.tsx
@@ -0,0 +1,49 @@
+"use client"
+
+import React from 'react';
+import Link from 'next/link';
+import { Tag } from '@/models/tag';
+import { Button } from '@nextui-org/react';
+import { deleteTag } from '@/actions/tags';
+import { usePathname, useRouter } from 'next/navigation';
+import generateColorFromText from '@/utils/generateColorFromText';
+
+const TagItem = ({tag} : {tag: Tag}) => {
+  const router = useRouter()
+  const pathname = usePathname();
+
+  const deleteTagHandler = async(id: string) => {
+    if(confirm('Are you sure you want to delete this tag') === true){
+      await deleteTag(id);
+      router.refresh()
+    }
+  }
+
+  return (
+    <li 
+    key={tag._id} 
+    className='flex items-center justify-between group' 
+    >
+      <Link 
+      href={'/tags/' + tag.name} 
+      className={`items-center flex flex-1 gap-x-2 p-1.5 ${pathname === `tags/${tag.name}` ? 'bg-gray-200 rounded' : ''}`}
+      >
+        <i 
+        style={{ color: generateColorFromText(tag.name)}} 
+        className={`ri-bookmark-fill text-xl`}
+        >  
+        </i>
+        <span className=''>{tag.name}</span>
+      </Link>
+    <Button
+    onClick={() => deleteTagHandler(tag._id!)} 
+    isIconOnly 
+    className={tag.createdBy ? 'bg-transparent hidden group-hover:block' : 'hidden'}
+    >
+      <i className="ri-delete-bin-line text-base text-foreground"></i>
+    </Button>
+    </li>
+  )
+}
+
+export default TagItem
