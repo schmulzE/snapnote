1e4ecf81a34d39090d3e6b92706de51918c76d89
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-11-21 22:20:05 +0100

    feat: Implemented Folder creation and edit form (FolderForm)
    * Created FolderForm component for adding and editing folders.
    * Handles form submission for creating new folders and updating existing ones.
    * Uses handleFolder action to interact with folder management logic.
    * Fetches folder details for editing using getFolder action and useEffect.
    * Handles form reset and closure on cancel button click.
    * Uses NextUI components for form elements and buttons.

---
+++ app/components/folders/folderForm.tsx
@@ -0,0 +1,82 @@
+"use client"
+
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import  { Folder } from '@/models/folder';
+import {Input, Button} from "@nextui-org/react";
+import { getFolder, handleFolder } from '@/actions/folders';
+
+
+
+const FolderForm = ({ onClose, folderId } : {onClose : () => void, folderId: string} ) => {
+  const ref = useRef<HTMLFormElement>(null);
+  const [folder, setFolder] = useState<Folder>({name: '', tag: ''});
+
+
+  async function handleSubmit(formData: FormData) {
+    if (folderId) {
+      return handleFolder(formData, folderId); 
+    } else {
+     return handleFolder(formData);
+    }
+  }
+
+  const fetchFolder = useCallback( async(id: string) => {
+    const data = await getFolder(id)
+    setFolder(data);
+  }, []);
+
+  useEffect(() => {
+    if(folderId) {
+      fetchFolder(folderId)
+    }
+  }, [fetchFolder, folderId]);
+
+  const closeModalHandler = () => {
+    ref.current?.reset()
+    onClose();
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
+    const { name, value } = e.target;
+    setFolder(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  return (
+    <>
+      <form 
+      ref={ref} 
+      action={async (formData) => {
+        onClose(); 
+        ref.current?.reset(); 
+        await handleSubmit(formData); 
+      }} 
+      className='z-50 px-2'
+      >
+        <Input 
+          type="text" 
+          variant='underlined' 
+          label="name" 
+          required
+          name="name"
+          className='mb-6'
+          value={folder.name}
+          onChange={handleChange}
+        />
+
+        <div className='flex w-full gap-x-4 justify-end'>
+          <Button color="danger" variant="light" type='button' onClick={closeModalHandler} className=' uppercase rounded-none'>
+            cancel
+          </Button>
+          <Button color="primary" type='submit' className='uppercase rounded-none'>
+            submit
+          </Button>
+        </div>
+      </form>
+    </>
+  )
+}
+
+export default FolderForm
