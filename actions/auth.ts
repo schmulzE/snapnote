ddb83738c18cdedfb563fb162cf02da016b00489
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-11-21 20:46:43 +0100

    feat: Implement user creation and existence check functionalities * Added createUser function to create new users in the database. * Added userExist function to check if a user with a specific email already exists. * Used Zod for data validation, bcrypt for password hashing, and mongoose for interacting with MongoDB.

---
+++ actions/auth.ts
@@ -0,0 +1,46 @@
+"use server" 
+import { z } from 'zod';
+import bcrypt from "bcryptjs";
+import { connectMongoDB } from '@/lib/mongodb';
+import UserModel, { User } from '@/models/user';
+
+export const createUser = async(prevState: any, formData: FormData) => {
+  const schema = z.object({
+    name: z.string().min(3),
+    email: z.string().email(),
+    password: z.string().min(8),
+  })
+
+  const parse = schema.safeParse({
+    name : formData.get('name'),
+    email : formData.get('email'),
+    password: formData.get('password'),
+  });
+
+  if(!parse.success) {
+    return { message: 'data is not valid' };
+  }
+
+  const data = parse.data;
+
+  try {
+    await connectMongoDB();
+    const user = await userExist(data.email);
+
+    if (user) {
+      throw new Error ('User already exists')
+    }
+
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+    await UserModel.create({ ...data, password: hashedPassword });
+    return { message: 'User created successfully' }
+  } catch (error) {
+    return { message: 'failed to create user' };
+  }
+}
+
+export const userExist = async(email: string) => {
+  await connectMongoDB();
+  const user = await UserModel.findOne({ email }).select("_id") as User;
+  return user;
+}
