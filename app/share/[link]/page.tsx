a1e07978378ff5f33eb296ff0e6bb71d117e09c8
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-11-21 21:37:58 +0100

    feat: Implement functionality to render shared notes (page)
    * Created a new route handler function page for shared notes.
    * Uses URL parameters to access the shared link.
    * Retrieves the shared link data by URL using SharedLinkModel.
    * Fetches the corresponding note data based on the retrieved
    oteId.
    * Combines note title and content for initial content in the Editor component.
    * Renders the Editor component in read-only mode with the retrieved content.

---
+++ app/share/[link]/page.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { connectMongoDB } from '@/lib/mongodb';
+import SharedLinkModel, { SharedLink } from '@/models/sharedLink';
+import Editor from '@/app/components/notes/noteEditor/editor';
+import NoteModel, { Note } from '@/models/note';
+
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+const BASE_URL = isDevelopment
+  ? process.env.DEV_BASE_URL || 'http://localhost:3000'
+  : process.env.PROD_BASE_URL || 'https://your-production-domain.com';
+
+const page = async({ params } : { params: { link: string } }) => {
+  const url = BASE_URL + '/share/' + params.link;
+  await connectMongoDB();
+  const sharedLink = await SharedLinkModel.findOne({ url }) as SharedLink;
+  const note = await NoteModel.findById( { _id: sharedLink.noteId }) as Note
+  const content = note.title ? note.title + note.content : note.content;
+
+  return (
+    <>
+      <Editor editable={false} initialContent={content} noteId={undefined}/>
+    </>
+  )
+}
+
+export default page
