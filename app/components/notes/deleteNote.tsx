190b84c318ce60b0bed2f0ded216aa2508522633
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-11-22 01:09:00 +0100

    feat: Implemented DeleteAllNotes button with confirmation
    * Added DeleteAllNotes button to delete all notes.
    * Implemented confirmation dialog before deleting.
    * Triggers deleteAllNotes action on confirmation.
    * Refreshes the page after successful deletion.

---
+++ app/components/notes/deleteNote.tsx
@@ -0,0 +1,24 @@
+"use client"
+import React from 'react'
+import { deleteAllNotes } from '@/actions/notes';
+import { Button } from '@nextui-org/react';
+import { useRouter } from 'next/navigation';
+
+
+const DeleteNote = () => {
+  const router = useRouter()
+  const deleteAllNotesHandler = async() => {
+    if(confirm('Are you sure you want to delete all Note') === true) {
+      await deleteAllNotes()
+      router.refresh()
+    }
+  }
+  return (
+    <div className='flex justify-between items-center'>
+    <div>Delete all notes</div>
+    <Button color='danger' onClick={deleteAllNotesHandler}>Delete all</Button>
+  </div>
+  )
+}
+
+export default DeleteNote
