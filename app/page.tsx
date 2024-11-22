aade56f0734c6e2b347529d3794012fccf8f41c9
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-11-22 13:41:35 +0100

    added the hero section, features, testimonial, pricing and suscribing section to the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,20 @@
-import Image from "next/image";
-import Navbar from "@/app/(components)/navbar";
-import Footer from "@/app/(components)/footer";
+import Navbar from "@/app/components/shared/navbar";
+import Footer from "@/app/components/shared/footer";
+import HeroSection from "./components/home/heroSection";
+import Subscribe from "./components/home/subscribe";
+import Features from "./components/home/features";
+import Pricing from "./components/home/pricing";
+import Testimonial from "./components/home/testimonials";
 
 export default function Home() {
   return (
     <>
       <Navbar/>
-        home
+      <HeroSection/>
+      <Features/>
+      <Testimonial/>
+      <Pricing/>
+      <Subscribe/>
       <Footer/>
     </>
   );
