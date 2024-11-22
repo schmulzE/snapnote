87d51f013db593ecce4b0128fae214b01de661f3
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-11-22 21:26:39 +0100

    Connected to the MongoDB database and fetched tags to populate the sidebar. Used Next-Themes for theme management. Wrapped the main content in a SidebarProvider for sidebar context.

--- app/search/layout.tsx
+++ app/search/layout.tsx
@@ -1,16 +1,26 @@
+import Tag from '@/models/tag';
 import React, { ReactNode } from 'react';
-import Sidebar from '../(components)/sidebar';
+import { ThemeProvider } from 'next-themes';
+import { connectMongoDB } from '@/lib/mongodb';
+import Sidebar from '../components/shared/sidebar';
+import { SidebarProvider } from '../sidebarProvider';
 
 
-const Layout = ({children} : {children : ReactNode}) => {
+const Layout = async({children} : {children : ReactNode}) => {
 
+  await connectMongoDB();
+  const tags = await Tag.find();
   return (
-    <div className='flex h-screen w-full z-50 overflow-hidden'>
-      <Sidebar/>
-      <div className='w-full px-4 overflow-auto'>
-        {children}
+    <SidebarProvider>
+      <div className='flex h-screen w-full z-50 overflow-hidden'>
+        <Sidebar tags={JSON.parse(JSON.stringify(tags))}/>
+        <div className='w-full px-4 overflow-auto'>
+          <ThemeProvider>
+            {children}
+          </ThemeProvider>
+        </div>
       </div>
-    </div>
+    </SidebarProvider>
   )
 }
 
