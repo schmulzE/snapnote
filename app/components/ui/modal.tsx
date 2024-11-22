88263564cea5bdfce2ff5e78b1ab4ba051492454
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-11-22 12:06:16 +0100

    feat: Implemented a reusable Modal component
    Implemented a reusable Modal component using NextUI's Modal component.
    Provided options for customizing the modal's size and title.
    Added a close button to the modal header.
    Supported dynamic content within the modal body.

---
+++ app/components/ui/modal.tsx
@@ -0,0 +1,38 @@
+import React, { ReactNode } from "react";
+import {Modal, ModalContent, ModalHeader, ModalBody } from "@nextui-org/react";
+
+
+type variant = "xs" | "sm" | "md" | "lg" | "xl" | "2xl" | "3xl" | "4xl" | "5xl" | "full" | undefined
+
+interface ModalProp { 
+  children: ReactNode, 
+  onClose: () => void, 
+  isOpen: boolean, 
+  title: string, 
+  size: variant 
+}
+
+export default function App({children, isOpen, onClose, title, size = "xs" } : ModalProp) {
+
+  return (
+    <>
+      <Modal 
+        size={size} 
+        isOpen={isOpen} 
+        onClose={onClose}
+        className="z-50" 
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">{title}</ModalHeader>
+              <ModalBody className="px-2">
+                {children}
+              </ModalBody>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
