a16c8b3f87dfba0564e8110f380ccd3dced61b24
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-11-22 01:20:33 +0100

    feat: Implemented Toolbar component with actions and filtering
    * Created Toolbar component to display title, actions, and filters.
    * Added buttons for adding new items (notes or folders).
    * Implemented delete and favorite actions with confirmation.
    * Integrated filter and sort components.
    * Added a share button for notes.
    * Used Popover for action menus.
    * Implemented responsive design for different screen sizes.

---
+++ app/components/shared/toolbar.tsx
@@ -0,0 +1,199 @@
+"use client";
+
+import React, { Dispatch, SetStateAction, useEffect, useReducer, useState } from 'react';
+import { Button } from '@nextui-org/react';
+import Popover from '@/app/components/ui/popover';
+import { Listbox, ListboxItem } from "@nextui-org/listbox";
+import { usePathname, useRouter } from "next/navigation";
+import Modal from '../ui/modal';
+import SelectTag from './selectTag';
+import SortComponent from './sortComponent';
+import FilterComponent from './filterComponent';
+import Breadcrumb from '../ui/breadcrumb';
+import FolderForm from '../folders/folderForm';
+import { Tag } from '@/models/tag';
+import { Folder } from '@/models/folder';
+import { Note } from '@/models/note';
+import { getTags } from '@/actions/tags';
+import {Tooltip} from "@nextui-org/tooltip";
+import ShareNoteForm from './shareNoteForm';
+import ToggleSidebarButton from './toggleSidebarButton';
+import ListboxWrapper from '@/app/components/ui/listBoxWrapper';
+import { addNoteToFavourite, deleteNote } from '@/actions/notes';
+// import { FacebookShareButton, TwitterShareButton } from "react-share";
+import { addFolderToFavourite, deleteFolder } from '@/actions/folders';
+import { initialState, modalReducer } from '@/app/reducers/modalReducer';
+
+function PopoverContent({deleteHandler, isFavourite, toggleFavourite} : {deleteHandler: () => void, isFavourite: boolean | undefined, toggleFavourite: () => void}) {
+
+  return (
+    <ListboxWrapper>
+      <Listbox aria-label="Actions">
+        <ListboxItem key="favourites" onPress={toggleFavourite}>
+          {isFavourite ? 'Remove from favourite' : 'Add to favourite'}
+        </ListboxItem>
+        <ListboxItem onPress={deleteHandler} key="delete" className="text-danger" color="danger">
+          Delete
+        </ListboxItem>
+      </Listbox>
+    </ListboxWrapper>
+  );
+}
+
+interface ToolbarProps{
+  title: string;
+  id: string;
+  isFavourite: boolean | undefined;
+  tag?: string;
+  url?: string;
+  setState?: Dispatch<SetStateAction<Note[] | Folder[]>>;
+  folderName?: string;
+  setFilterByFavorite?: Dispatch<SetStateAction<boolean>>;
+  setFilterByNoTag?: Dispatch<SetStateAction<boolean>>
+  setSortAlphabetically?: Dispatch<SetStateAction<boolean>>
+  setSortByDate?: Dispatch<SetStateAction<boolean>>
+}
+
+const Toolbar = (
+  { title, 
+    id, 
+    tag, 
+    isFavourite, 
+    setState, 
+    folderName, 
+    url, 
+    setFilterByFavorite, 
+    setFilterByNoTag, 
+    setSortAlphabetically, 
+    setSortByDate
+  } : ToolbarProps) => {
+  const [ formData , setFormData] = useState<Note>({favourite: isFavourite});
+  const [tags, setTags] = useState<Tag[]>([]);
+  const router = useRouter();
+  const pathname = usePathname();
+  const [state, dispatch] = useReducer(modalReducer, initialState);
+
+  const DYNAMICROUTE = pathname === `/${title}/${id}`;
+  const SINGLEROUTE = pathname === `/${title}`
+
+  const openModal = (title: string, content: React.ReactNode) => {
+    dispatch({ type: 'OPEN_MODAL', title, content });
+  };
+
+  const closeModal = () => {
+    dispatch({ type: 'CLOSE_MODAL' });
+  };
+
+  async function fetchTags() {
+    const data = await getTags()
+    setTags(data); 
+  };
+  
+  useEffect(() => {
+    fetchTags();
+  }, []);
+
+
+  const deleteHandler = async () => {
+    if(confirm('Are you sure want to delete') === true) {
+      title === 'folders' ? await deleteFolder(id) : await deleteNote(id)
+      router.refresh();
+      router.push(`/${title}`);
+    }
+  };
+
+  const toggleFavourite = async() => {
+    const updatedFormData = { ...formData, favourite: !formData.favourite };
+    setFormData(updatedFormData);
+    title === "folders" ? addFolderToFavourite(id, !formData.favourite) : addNoteToFavourite(id, !formData.favourite);
+  }
+
+
+  return (
+    <>
+      <div className="flex justify-between px-0 lg:px-4 items-center capitalize my-3">
+        <div className='flex items-center gap-2'>
+          <ToggleSidebarButton/>
+          <h1 className='text-xl lg:text-4xl font-bold capitalize'>{title}</h1>
+          { SINGLEROUTE && title === 'folders' ? 
+            <Tooltip radius='none' content='Add new folder'>
+              <Button 
+              size='sm' 
+              onClick={() => openModal('Add new folder', <FolderForm onClose={closeModal} folderId=''/>)} 
+              className='border bordered-gray-200 bg-transparent flex items-center'
+              >
+                <i className='ri-add-fill text-lg'></i>
+                Add folder
+              </Button>
+            </Tooltip>
+           : null
+          }
+        </div>
+
+         { DYNAMICROUTE && title === 'folders' ? <Breadcrumb folderName={folderName}/> : null }
+
+        <div className='space-x-3'>
+          { SINGLEROUTE && 
+          <Popover 
+          button={
+            <Button isIconOnly className='outline-none bg-transparent'>
+              <i className="ri-filter-3-line text-xl"></i>
+            </Button>
+          } 
+          content={<FilterComponent setFilterByNoTag={setFilterByNoTag} setFilterByFavorite={setFilterByFavorite} />}/>
+          }
+    
+          { SINGLEROUTE && 
+            <Popover 
+            button={
+              <Button isIconOnly className='outline-none bg-transparent'>
+                <i className="ri-sort-asc text-xl"></i>
+              </Button>
+            } 
+            content={<SortComponent setSortByDate={setSortByDate!} setSortAlphabetically={setSortAlphabetically!} setState={setState} />}
+            /> 
+          }
+            
+          { DYNAMICROUTE && 
+            <Tooltip 
+            radius='none' 
+            content={'add tag'}>
+              <button
+              onClick={
+                () => openModal('Select Tag', <SelectTag text={title} tags={tags} onClose={closeModal} tagId={tag} id={id}/>)
+                } 
+              className='bg-transparent'
+              >
+                <i className='ri-bookmark-line'></i>
+              </button>
+            </Tooltip> 
+          }
+
+          { DYNAMICROUTE && 
+          <Popover 
+          button={<button className='outline-none bg-transparent'><i className="ri-more-2-fill"></i></button>} 
+          content={<PopoverContent deleteHandler={deleteHandler} toggleFavourite={toggleFavourite} isFavourite={formData.favourite}/>}
+          />
+          }
+           
+          { DYNAMICROUTE && title === 'notes' && 
+            <Button
+            className='text-foreground'
+            onClick={() => openModal('Share Note', <ShareNoteForm noteId={id} url={url!}/>)} 
+            color="primary"
+            > 
+            Share 
+            </Button>
+          }
+        </div>
+      </div>
+
+      <Modal isOpen={state.isOpen} onClose={closeModal} title={state.title} size={'sm'}>
+        {state.content}
+      </Modal>
+
+    </>
+  )
+}
+
+export default Toolbar;
