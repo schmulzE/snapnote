9f64bf9ef9495d19d9be878286549ea8bc440cdc
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-11-22 13:54:36 +0100

    re-designed the page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,20 @@
+import Image from 'next/image'
 import Link from 'next/link'
- 
+import EmptyFolder from '@/public/no-results.png';
+import Navbar from "@/app/components/shared/navbar";
+import Footer from "@/app/components/shared/footer";
+
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
-    </div>
+    <>
+      <Navbar/>
+      <div className='flex flex-col justify-center content-center items-center gap-y-4 my-28'>
+        <Image src={EmptyFolder} width={200} alt='image of an empty folder'/>
+        <h1 className='text-3xl'>Page Not Found</h1>
+        <p>Could not find requested resource</p>
+        <Link href="/" className='bg-gray-900 text-white p-2'>Return Home</Link>
+      </div>
+      <Footer/>
+    </>
   )
 }
