efdfe3b3e9148afd7c5788abf54aa65d2c861bae
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-11-22 01:38:34 +0100

    feat: Implemented Sidebar component for navigation and actions
    Implemented a Sidebar component to provide navigation and actions within the application.
    Used NavLinkGroup and NavLink components for organizing navigation links.
    Added a search input for filtering notes.
    Integrated a Tags component for displaying and managing tags.
    Implemented a responsive design to adjust the sidebar layout for different screen sizes.
    Used the useSidebar context for managing the mobile sidebar state.

---
+++ app/components/shared/sidebar.tsx
@@ -0,0 +1,59 @@
+'use client'
+import React from 'react';
+import {Tag } from '@/models/tag';
+import Tags from '../tags/tags';
+import SearchInput from './searchInput';
+import { Button } from '@nextui-org/react';
+import { NavLinkGroup, NavLink } from '../ui/navLinkGroup';
+import { useSidebar } from '@/app/sidebarProvider';
+
+const Sidebar =  ({ tags }: { tags: Tag[]}) => {
+  const { isMobile, isOpen, toggleSidebar } = useSidebar()
+
+  const sidebarContent = (
+    <div>
+      <div className='space-y-8 lg:space-y-4 w-full'>
+        <div className='flex items-center justify-between'>
+          <h1 className='text-xl font-medium'>Menu</h1>
+          <Button isIconOnly className='bg-transparent hidden lg:block'>
+            <i className='ri-menu-fill text-xl'></i>
+          </Button>
+        </div>
+
+        {!isMobile && <SearchInput/>}
+
+        <NavLinkGroup className="space-y-2 flex-col flex">
+          <NavLink to="/notes" icon={<i className="ri-file-3-line text-xl"></i>} text="Notes" />
+          <NavLink to="/todos" icon={<i className="ri-list-unordered text-xl"></i>} text="Todos" />
+          <NavLink to="/folders" icon={<i className="ri-folders-line text-xl"></i>} text="Folders" />
+          <NavLink to="/favourites" icon={<i className="ri-star-line text-xl"></i>} text="Favourites" />
+        </NavLinkGroup>
+
+        <Tags tags={tags }/>
+      </div>
+
+      <NavLinkGroup className="flex flex-col text-md gap-y-2 mb-2">
+        <NavLink to={''} icon={<i className="ri-logout-box-r-line text-xl"></i>} text="Logout" />
+        <NavLink to="/settings" icon={<i className="ri-settings-3-line text-xl"></i>} text="Settings" />
+      </NavLinkGroup>
+    </div>
+  )
+
+if (isMobile) {
+  return isOpen ? (
+    <div className="fixed inset-0 z-50 bg-black/30 backdrop-blur-sm w-full" onClick={toggleSidebar}>
+      <div className="fixed left-0 top-0 h-full w-64 max-w-xs bg-background flex flex-col justify-between p-4">
+        {sidebarContent}
+      </div>
+    </div>
+  ) : null
+}
+
+return (
+  <div className='hidden md:flex md:flex-col justify-between px-2 capitalize sticky top-0 w-96 pt-2 m-2 rounded-md z-50 text-sm bg-content1' >
+    {sidebarContent}
+  </div>
+)
+}
+
+export default Sidebar
