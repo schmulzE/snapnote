2a1a5dccc48ce7006fd9921a075d2fd76fa69040
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-11-22 01:35:19 +0100

    feat: Implemented SelectTag component for assigning tags to notes/folders
    * Implemented a SelectTag component to allow users to assign tags to notes or folders.
    * Added a custom Radio component (CustomRadio) for styled radio buttons displaying tag names and colors.
    * Users can select a tag from a list and submit the selection.
    * Implemented logic to handle tag selection changes and form submission.
    * Used selectNoteTag and selectFolderTag actions to update tags based on the selected note/folder ID.

---
+++ app/components/shared/selectTag.tsx
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import { Button, RadioGroup, Radio, cn, RadioProps } from "@nextui-org/react";
+import generateColorFromText from '@/utils/generateColorFromText';
+import { Tag } from '@/models/tag';
+import { selectFolderTag } from '@/actions/folders';
+import { selectNoteTag } from '@/actions/notes';
+
+interface CustomRadioProps extends RadioProps {
+  children: React.ReactNode;
+}
+
+export const CustomRadio = ({ children, ...otherProps }: CustomRadioProps) => {
+  return (
+    <Radio
+      {...otherProps}
+      classNames={{
+        base: cn(
+          "inline-flex m-0 bg-content1 hover:bg-content2 items-center justify-between",
+          "flex-row-reverse max-w-[300px] cursor-pointer rounded-lg gap-4 p-2 border-2 border-transparent",
+          "data-[selected=true]:border-primary capitalize"
+        ),
+      }}
+    >
+      {children}
+    </Radio>
+  );
+};
+
+const SelectTag = ({ tags, onClose, text, tagId, id }: { tags: Tag[], onClose: () => void, text: string, tagId?: string, id: string }) => {
+  const [selectedTag, setSelectedTag] = useState<string | undefined>(tagId);
+
+  const handleChange = (value: string) => {
+    setSelectedTag(value);
+    console.log(value);
+  }
+
+  const handleSubmit = async (formData: FormData) => {
+    if (!selectedTag) return;
+
+    formData.append('tag', selectedTag);
+
+    if (text === 'notes') {
+      return selectNoteTag(id, formData);
+    } else {
+      return selectFolderTag(id, formData);
+    }
+  }
+
+  return (
+    <form action={handleSubmit} className='w-full'>
+      <RadioGroup 
+        className='w-full' 
+        value={selectedTag} 
+        onValueChange={handleChange}
+      >
+        {tags.map(tag => (
+          <CustomRadio 
+            key={tag._id} 
+            className='w-full max-w-7xl' 
+            value={tag._id as string}
+          >
+            <i 
+              style={{ color: generateColorFromText(tag.name) }} 
+              className='ri-bookmark-fill text-xl inline-block mr-2'
+            ></i>
+            {tag.name} 
+          </CustomRadio>
+        ))}
+      </RadioGroup>
+
+      <div className='flex justify-end w-full mt-2 px-2 gap-2'>
+        <Button variant='bordered' type='button' color='default' onClick={onClose}>
+          Cancel
+        </Button>
+        <Button 
+          type='submit' 
+          className='bg-blue-500 text-white' 
+          isDisabled={!selectedTag}
+        >
+          Submit
+        </Button>
+      </div>
+    </form>
+  )
+}
+
+export default SelectTag
