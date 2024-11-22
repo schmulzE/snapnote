48106e19e7a06be52923b948dd5c7a22af3842a3
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-11-22 21:24:35 +0100

    Used MongoDB's aggregation pipeline to calculate the total notes per folder. Rendered the folders and their note counts using the FolderView component. Included a Suspense component with a loading indicator for a better user experience.

--- app/folders/page.tsx
+++ app/folders/page.tsx
@@ -1,30 +1,34 @@
-import React from 'react';
-import Folders from '../(components)/folders';
-
-async function getfolders() {
-  try {
-    const res = await fetch(`http://localhost:3000/api/folders`,  {
-      cache: "no-store",
-    });
-   
-    if (!res.ok) {
-      throw new Error('Failed to fetch data')
-    }
-   
-    return res.json()
-    
-  } catch (error) {
-    console.log(error)
-  }
-}
+import React, { Suspense } from 'react';
+import { Spinner } from '@nextui-org/react';
+import { connectMongoDB } from '@/lib/mongodb';
+import FolderModel, { Folder } from '@/models/folder';
+import FolderView from '../components/folders/folderView';
 
 const page = async() => {
-  const data = await getfolders()
-  const folders = data.folders
+  await connectMongoDB();
+  const data = await FolderModel.aggregate([
+    {
+      $lookup: {
+        from: 'notes',
+        localField: '_id',
+        foreignField: 'folder',
+        as: 'notes',
+      },
+    },
+    {
+      $addFields: {
+        totalNotes: { $size: '$notes' },
+      },
+    },
+  ]);
+
+  const folders = JSON.parse(JSON.stringify(data)) as Folder[];
 
   return (
     <div>
-      <Folders folders={folders}/>
+      <Suspense fallback={<div className='flex justify-center content-center'><Spinner label="Default" color="default" labelColor="foreground"/></div>}>
+        <FolderView folders={folders}/>
+      </Suspense>
     </div>
   )
 }
