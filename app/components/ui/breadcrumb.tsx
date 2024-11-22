f898725346ab9d65adfa7dff90be04a6bd7be20f
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-11-22 11:59:25 +0100

    feat: Implemented Breadcrumb component for displaying folder hierarchy
    Implemented a Breadcrumb component to display the current folder's location in the hierarchy.
    Used NextUI's Breadcrumb component for the breadcrumb structure.
    Included folder name and icons for visual clarity.

---
+++ app/components/ui/breadcrumb.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import { BreadcrumbItem, Breadcrumbs } from '@nextui-org/react';
+import React from 'react';
+
+const Breadcrumb = ({folderName} : { folderName : string | undefined}) => {
+  return (
+    <>
+     <Breadcrumbs>
+      <BreadcrumbItem startContent={<i className="ri-folder-line"/>} endContent={<i className="ri-arrow-right-s-line"></i>}>
+        <span>{folderName}</span>
+      </BreadcrumbItem>
+      </Breadcrumbs>
+    </>
+  )
+}
+
+export default Breadcrumb
