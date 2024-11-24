e77ffd741e536d8b745607855aab4760bb9e13a2
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-11-25 00:47:47 +0100

    feat: Implemented Settings Page with Theme Switcher, Shared Links, and Delete Note
    - Added App component to render the Settings page.
    - Fetched shared links from MongoDB.
    - Rendered ThemeSwitcher, SharedLinkContainer, and DeleteNote components.

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import ThemeSwitcher from '../components/shared/themeSwitcher';
+import SharedLinkContainer from '../components/sharedLinks/sharedLinkContainer';
+import { connectMongoDB } from '@/lib/mongodb';
+import SharedLinkModel, { SharedLink } from '@/models/sharedLink';
+import DeleteNote from '../components/notes/deleteNote';
+import ToggleSidebarButton from '../components/shared/toggleSidebarButton';
 
-const page = () => {
+export default async function App() {
+  await connectMongoDB();
+  const sharedLinks = (await SharedLinkModel.find()) as SharedLink[];
+
   return (
-    <div>
-      settings
-    </div>
+    <>
+      <div className="flex items-center my-4">
+        <ToggleSidebarButton/>
+        <h1 className='text-xl md:text-3xl font-medium'>Settings</h1>
+      </div>
+      <div className="bg-content1 w-full max-5xl rounded-md p-4 space-y-4 text-sm">
+        <ThemeSwitcher/>
+        <SharedLinkContainer sharedLinks={JSON.parse(JSON.stringify(sharedLinks))}/>
+        <DeleteNote/>
+      </div>
+    </>
   )
 }
-
-export default page
