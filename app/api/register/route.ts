360b8d63d51614f2779596cf20b9fcc4731f7f79
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-11-25 21:51:35 +0100

    implemented better error handling

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,18 +1,18 @@
-import { connectMongoDB } from "@/lib/mongodb";
-import User from "@/models/user";
-import { NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
+import User from "@/models/user";
+import { type NextRequest } from "next/server";
+import { connectMongoDB } from "@/lib/mongodb";
 
-export async function POST(req: NextResponse) {
+export async function POST(req: NextRequest) {
   try {
     const { name, email, password } = await req.json();
     const hashedPassword = await bcrypt.hash(password, 10);
     await connectMongoDB();
     await User.create({ name, email, password: hashedPassword });
 
-    return NextResponse.json({ message: "User registered." }, { status: 201 });
+    return Response.json({ message: "User registered." }, { status: 201 });
   } catch (error) {
-    return NextResponse.json(
+    return Response.json(
       { message: "An error occurred while registering the user." },
       { status: 500 }
     );
