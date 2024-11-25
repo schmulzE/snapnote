9520201e6ebffa44bceba5c4a95b4ba42c1b58f8
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-11-25 21:48:14 +0100

    feat: Populate session.user.id with database user Id

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,9 +3,11 @@ import User from "@/models/user";
 import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
-import { Session } from "next-auth";
+import { Session, AuthOptions, DefaultSession } from "next-auth";
+import { JWT } from "next-auth/jwt";
+import { AdapterUser } from "next-auth/adapters";
 
-export const authOptions = {
+export const authOptions: AuthOptions = {
   providers: [
     CredentialsProvider({
       name: "credentials",
@@ -46,16 +48,19 @@ export const authOptions = {
     signIn: "/",
   },
   callbacks: {
-    async session({ session }: { session: Session }) {
-      const sessionUser = await User.findOne({ email: session.user.email });
-      if (sessionUser) {
-        session.user.id = sessionUser._id.toString();
-        return session;                
-      } 
+    async session({ session, token, user }: { session: Session; token: JWT; user: AdapterUser }) {
+      if (session?.user?.email) {
+        await connectMongoDB();
+        const sessionUser = await User.findOne({ email: session.user.email });
+        if (sessionUser) {
+          session.user.id = sessionUser._id.toString();
+        }
+      }
+      return session as Session | DefaultSession;
     },
   },
 };
 
-const handler = NextAuth(authOptions as any);
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
