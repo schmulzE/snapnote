4c7cd7cf6ef79dc3c2f37c8fe3c581b98eaac71d
wisdom eshiet <wisdomeshiet65@gmail.com>
2025-06-06 19:53:20 +0100

    Added temporary logging for MONGODB_URI in lib/mongodb.ts dded temporary logging for NEXTAUTH_SECRET in app/api/auth/[...nextauth]/auth.ts

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectMongoDB = async () => {
+  console.log("MONGODB_URI:", process.env.MONGODB_URI);
   try {
     await mongoose.connect(process.env.MONGODB_URI!);
     console.log("Connected to MongoDB");
