933c7a4fb252f86d66dc21013824127de0239f6f
wisdom eshiet <wisdomeshiet65@gmail.com>
2025-06-07 03:40:03 +0100

    removed white spaces

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -24,25 +24,3 @@ const Page = async () => {
 }
  
 export default Page;
-
-// import { Suspense } from 'react';
-// import { getNotes } from '@/actions/notes';
-// import { Spinner } from '@nextui-org/react';
-// import NoteViewer from '@/app/components/notes/notesViewer';
-
-// const Page = async () => {
-
-//   return (
-//     <>
-//       <Suspense fallback={
-//         <div className='flex justify-center h-full content-center'>
-//           <Spinner label="Loading..." color="default" labelColor="foreground"/>
-//         </div>
-//       }>
-//         <NoteViewer fetchNotes={getNotes} title={'notes'} />
-//       </Suspense>
-//     </>
-//   )
-// }
- 
-// export default Page;
